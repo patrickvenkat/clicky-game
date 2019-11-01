631314e22053ed4baa0f39e3335c3b3b20125876
Patrick Venkat <patrick.venkat@gmail.com>
2019-10-31 20:50:24 -0400

    Updates

---
+++ precache-manifest.61a2ba903e64792f72c8f7233072c317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daf96ec1f9f0b4dc31b4ce746c8a4a1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ff490b13c4394b2e4666",
+    "url": "/clicky-game/static/css/main.449b2bb7.chunk.css"
+  },
+  {
+    "revision": "5fb8e11d443e357d9fe7",
+    "url": "/clicky-game/static/js/2.13ed72e9.chunk.js"
+  },
+  {
+    "revision": "ff490b13c4394b2e4666",
+    "url": "/clicky-game/static/js/main.1e6d55e0.chunk.js"
+  },
+  {
+    "revision": "c2f03ffa8bd2110d30e4",
+    "url": "/clicky-game/static/js/runtime-main.e30a1ab2.js"
+  }
+]);
